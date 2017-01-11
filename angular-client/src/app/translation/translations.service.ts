bec37464c191f94943a304c07f3d4b00c565e76e
Grant <gcg4wg@virginia.edu>
2017-01-11 16:44:37 -0500

    change rxjs import

--- angular-client/src/app/translation/translations.service.ts
+++ angular-client/src/app/translation/translations.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
-import { Observable } from 'rxjs/Rx';
+
 import 'rxjs/add/operator/map'
 
 import { Translation } from './translation.model';
